8d9e05eea6a5c3b010439337f5fb6a5fcf079054
ashiqur_sami <ashiqur.rahman@bjitacademy.com>
2024-11-29 17:15:33 +0600

    deleted

--- ashiqur_rahman_sami_30206_django_REST/employee/models.py
+++
@@ -1,9 +0,0 @@
-from django.db import models
-
-class Employee(models.Model):
-    name = models.CharField(max_length=50)
-    email = models.EmailField(unique=True,null=True,blank=True)
-    position = models.CharField(max_length=50)
-
-    def __str__(self):
-        return f"{self.first_name} {self.last_name}"
